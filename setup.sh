a50992a0c36a4b740ba7f2f63c1afc7fb055ea03
moonsoul38 <moonsoul388@outlook.com>
2026-06-02 20:38:41 +0800

    Modify setup.sh to apply gunyah patch
    Backup the original module root, apply a patch to gunyah_qcom.c, and restore the module root.

--- setup.sh
+++ setup.sh
@@ -3,3 +3,9 @@ set -euo pipefail
 
 echo "Running custom module from: $PWD"
 echo "Kernel root: $KERNEL_ROOT"
+
+module_root_bak=$module_root
+module_root=$PWD
+cd $KERNEL_ROOT/drivers/virt/gunyah/
+patch gunyah_qcom.c $module_root/fix_8gen3_gunyah.patch
+module_root=$module_root_bak
