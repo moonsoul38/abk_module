f9b57e69cec00c4e29cb914b504dd7ac4ffd285d
moonsoul38 <moonsoul388@outlook.com>
2026-06-03 18:59:57 +0800

    Update patches in setup.sh for gunyah drivers

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 echo "Running custom module from: $PWD"
 echo "Kernel root: $KERNEL_ROOT"
+echo "内核版本：$CONFIG"
 
-patch $KERNEL_ROOT/common/drivers/virt/gunyah/gunyah_qcom.c fix_8gen3_gunyah.patch
-
+patch $KERNEL_ROOT/common/drivers/virt/gunyah/gunyah_qcom.c fix_8gen3_gunyah_1.patch
+patch $KERNEL_ROOT/common/drivers/virt/gunyah/vm_mgr_mm.c fix_8gen3_gunyah_2.patch
