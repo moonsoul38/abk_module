dec4ff819f162e86fc098dc158666bc5b4679f7d
moonsoul38 <moonsoul388@outlook.com>
2026-06-03 19:52:51 +0800

    应用patch3

--- setup.sh
+++ setup.sh
@@ -7,3 +7,4 @@ echo "内核版本：$CONFIG"
 
 patch $KERNEL_ROOT/common/drivers/virt/gunyah/gunyah_qcom.c patches/fix_8gen3_gunyah_1.patch
 patch $KERNEL_ROOT/common/drivers/virt/gunyah/vm_mgr_mm.c patches/fix_8gen3_gunyah_2.patch
+patch $KERNEL_ROOT/common/drivers/virt/gunyah/gunyah_qcom.c patches/fix_8gen3_gunyah_3.patch
