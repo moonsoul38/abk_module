79c8992c9826de441ba112e44b5b3c665f2aa5b3
moonsoul38 <moonsoul388@outlook.com>
2026-06-03 18:29:22 +0800

    Simplify patch command in setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,5 @@ set -euo pipefail
 echo "Running custom module from: $PWD"
 echo "Kernel root: $KERNEL_ROOT"
 
-#module_root_bak=$module_root
-module_root=$PWD
-cd $KERNEL_ROOT/common/drivers/virt/gunyah/
-patch gunyah_qcom.c $module_root/fix_8gen3_gunyah.patch
-#module_root=$module_root_bak
+patch $KERNEL_ROOT/common/drivers/virt/gunyah/gunyah_qcom.c fix_8gen3_gunyah.patch
+
