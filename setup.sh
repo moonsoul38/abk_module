8b85c5b063e03c11df5dd7a0a95497c73ef6b409
moonsoul38 <moonsoul388@outlook.com>
2026-06-03 19:01:05 +0800

    修正setup.sh路径

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ echo "Running custom module from: $PWD"
 echo "Kernel root: $KERNEL_ROOT"
 echo "内核版本：$CONFIG"
 
-patch $KERNEL_ROOT/common/drivers/virt/gunyah/gunyah_qcom.c fix_8gen3_gunyah_1.patch
-patch $KERNEL_ROOT/common/drivers/virt/gunyah/vm_mgr_mm.c fix_8gen3_gunyah_2.patch
+patch $KERNEL_ROOT/common/drivers/virt/gunyah/gunyah_qcom.c patches/fix_8gen3_gunyah_1.patch
+patch $KERNEL_ROOT/common/drivers/virt/gunyah/vm_mgr_mm.c patches/fix_8gen3_gunyah_2.patch
