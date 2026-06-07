00365eb82862068b7efe0b14528de51574446e64
moonsoul38 <moonsoul388@outlook.com>
2026-06-07 18:17:09 +0800

    Add kernel version support check in setup.sh
    Add support check for kernel version and update patching logic.

--- setup.sh
+++ setup.sh
@@ -4,7 +4,13 @@ set -euo pipefail
 echo "Running custom module from: $PWD"
 echo "Kernel root: $KERNEL_ROOT"
 echo "内核版本：$CONFIG"
+if [[ $CONFIG = android14-6.1-* ]]; then
+  echo '当前内核版本受支持'
+else
+  echo '不支持的内核版本'
+  exit 1
+fi
 
 patch $KERNEL_ROOT/common/drivers/virt/gunyah/gunyah_qcom.c patches/fix_8gen3_gunyah_1.patch
 patch $KERNEL_ROOT/common/drivers/virt/gunyah/vm_mgr_mm.c patches/fix_8gen3_gunyah_2.patch
-patch $KERNEL_ROOT/common/drivers/virt/gunyah/gunyah_qcom.c patches/fix_8gen3_gunyah_3.patch
+#patch $KERNEL_ROOT/common/drivers/virt/gunyah/gunyah_qcom.c patches/fix_8gen3_gunyah_3.patch
